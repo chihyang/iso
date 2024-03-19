6a0b5e56a7b3c132f295afda9c96084118058151
chihyang <chihyanghsin@gmail.com>
2024-03-19 12:18:08 -0400

    Use Maybe in place of error in linear checker.

--- src/LinearCheck.hs
+++ src/LinearCheck.hs
@@ -3,106 +3,115 @@ module LinearCheck (linearCheck) where
 import Syntax
 
 type LinearEnv = [(String , Int)]
+type Result a = Maybe a
 
-linearCheck :: LinearEnv -> Program -> LinearEnv
-linearCheck env (PgTm tm) = linearCheckTm env tm
-linearCheck env (PgIs iso) = linearCheckIso env iso
+linearCheck :: Program -> Result LinearEnv
+linearCheck (PgTm tm) = lcTm [] tm
+linearCheck (PgIs iso) = lcIso [] iso
 
 {---------- Linear checking for Terms ----------}
-linearCheckTm :: LinearEnv -> Term -> LinearEnv
-linearCheckTm env TmUnit = env
-linearCheckTm env (TmInt _) = env
-linearCheckTm env (TmVar var) = increEnv env var
-linearCheckTm env (TmLInj tm) = linearCheckTm env tm
-linearCheckTm env (TmRInj tm) = linearCheckTm env tm
-linearCheckTm env (TmPair l r) = env'' where
-  env' = linearCheckTm env l
-  env'' = linearCheckTm env' r
-linearCheckTm env (TmIsoApp iso tm) = linearCheckTm env' tm where
-  env' = linearCheckIso env iso
-linearCheckTm env (TmLet pat rhs body) = env'''' where
-  env' = linearCheckTm env rhs
-  env'' = extendPat env' pat
-  env''' = linearCheckTm env'' body
-  env'''' = dropPat env''' pat
-linearCheckTm env (TmAnn tm _) = linearCheckTm env tm
+lcTm :: LinearEnv -> Term -> Result LinearEnv
+lcTm env TmUnit = return env
+lcTm env (TmInt _) = return env
+lcTm env (TmVar var) = increEnv env var
+lcTm env (TmLInj tm) = lcTm env tm
+lcTm env (TmRInj tm) = lcTm env tm
+lcTm env (TmPair l r) = do
+  env' <- lcTm env l
+  lcTm env' r
+lcTm env (TmIsoApp iso tm) = do
+  env' <- lcIso env iso
+  lcTm env' tm
+lcTm env (TmLet pat rhs body) = do
+  env' <- lcTm env rhs
+  env'' <- extendPat env' pat
+  env''' <- lcTm env'' body
+  dropPat env''' pat
+lcTm env (TmAnn tm _) = lcTm env tm
 
 {---------- Linear checking for Isos ----------}
-linearCheckIso :: LinearEnv -> Iso -> LinearEnv
-linearCheckIso env (IsoValue pairs) = linearCheckIsoPairs env pairs
-linearCheckIso env (IsoVar _) = env
+lcIso :: LinearEnv -> Iso -> Result LinearEnv
+lcIso env (IsoValue pairs) = lcIsoPairs env pairs
+lcIso env (IsoVar _) = return env
 -- iso var binding is ignored
-linearCheckIso env (IsoLam _ _ _ body) = linearCheckIso env body
-linearCheckIso env (IsoApp rator rand) = linearCheckIso env' rator where
-  env' = linearCheckIso env rand
-linearCheckIso _ (IsoFix _ _) = error $ "IsoFix is not supported yet!"
-linearCheckIso env (IsoAnn iso _) = linearCheckIso env iso
+lcIso env (IsoLam _ _ _ body) = lcIso env body
+lcIso env (IsoApp rator rand) = do
+  env' <- lcIso env rand
+  lcIso env' rator
+lcIso _ (IsoFix _ _) = error $ "IsoFix is not supported yet!"
+lcIso env (IsoAnn iso _) = lcIso env iso
 
-linearCheckIsoPairs :: LinearEnv -> [(Value, Exp)] -> LinearEnv
-linearCheckIsoPairs env [] = env
-linearCheckIsoPairs env (hd:tl) = linearCheckIsoPairs env' tl where
-  env' = linearCheckIsoPair env hd
+lcIsoPairs :: LinearEnv -> [(Value, Exp)] -> Result LinearEnv
+lcIsoPairs env [] = return env
+lcIsoPairs env (hd:tl) = do
+  env' <- lcIsoPair env hd
+  lcIsoPairs env' tl
 
-linearCheckIsoPair :: LinearEnv -> (Value, Exp) -> LinearEnv
-linearCheckIsoPair env (v, e) = env'' where
-  vars = linearCheckValue env v
-  env' = extendPat env (PtMultiVar vars)
-  env'' = linearCheckExp env' e
+lcIsoPair :: LinearEnv -> (Value, Exp) -> Result LinearEnv
+lcIsoPair env (v, e) = do
+  let vars = lcVal env v
+  env' <- extendPat env (PtMultiVar vars)
+  lcExp env' e
 
 {---------- Linear type checking for Values -----------}
-linearCheckValue :: LinearEnv -> Value -> [String]
-linearCheckValue _ ValUnit = []
-linearCheckValue _ (ValInt _) = []
-linearCheckValue _ (ValVar var) = [var]
-linearCheckValue env (ValLInj v) = linearCheckValue env v
-linearCheckValue env (ValRInj v) = linearCheckValue env v
-linearCheckValue env (ValPair l r) = lVars ++ rVars where
-  lVars = linearCheckValue env l
-  rVars = linearCheckValue env r
-linearCheckValue env (ValAnn v _) = linearCheckValue env v
+lcVal :: LinearEnv -> Value -> [String]
+lcVal _ ValUnit = []
+lcVal _ (ValInt _) = []
+lcVal _ (ValVar var) = [var]
+lcVal env (ValLInj v) = lcVal env v
+lcVal env (ValRInj v) = lcVal env v
+lcVal env (ValPair l r) = lVars ++ rVars where
+  lVars = lcVal env l
+  rVars = lcVal env r
+lcVal env (ValAnn v _) = lcVal env v
 
 {---------- Linear type checking for Exps -----------}
-linearCheckExp :: LinearEnv -> Exp -> LinearEnv
-linearCheckExp env (ExpVal val) = linearCheckValueNoPattern env val
-linearCheckExp env (ExpLet pat iso pat' body) = env''''' where
-  env' = linearCheckIso env iso
-  env'' = linearCheckRhsPattern env' pat'
-  env''' = extendPat env'' pat
-  env'''' = linearCheckExp env''' body
-  env''''' = dropPat env'''' pat
+lcExp :: LinearEnv -> Exp -> Result LinearEnv
+lcExp env (ExpVal val) = lcValNoPat env val
+lcExp env (ExpLet pat iso pat' body) = do
+  env' <- lcIso env iso
+  env'' <- lcRhsPat env' pat'
+  env''' <- extendPat env'' pat
+  env'''' <- lcExp env''' body
+  dropPat env'''' pat
 
-linearCheckRhsPattern :: LinearEnv -> Pattern -> LinearEnv
-linearCheckRhsPattern env (PtSingleVar var) = increEnv env var
-linearCheckRhsPattern env (PtMultiVar vars) = foldl increEnv env vars
+lcRhsPat :: LinearEnv -> Pattern -> Result LinearEnv
+lcRhsPat env (PtSingleVar var) = increEnv env var
+lcRhsPat env (PtMultiVar []) = return env
+lcRhsPat env (PtMultiVar (var:vars)) = do
+  env' <- increEnv env var
+  lcRhsPat env' (PtMultiVar vars)
 
 {-- Linear type checking for Values (Non-pattern) --}
-linearCheckValueNoPattern :: LinearEnv -> Value -> LinearEnv
-linearCheckValueNoPattern env ValUnit = env
-linearCheckValueNoPattern env (ValInt _) = env
-linearCheckValueNoPattern env (ValVar var) = increEnv env var
-linearCheckValueNoPattern env (ValLInj v) = linearCheckValueNoPattern env v
-linearCheckValueNoPattern env (ValRInj v) = linearCheckValueNoPattern env v
-linearCheckValueNoPattern env (ValPair l r) = env'' where
-  env' = linearCheckValueNoPattern env l
-  env'' = linearCheckValueNoPattern env' r
-linearCheckValueNoPattern env (ValAnn v _) = linearCheckValueNoPattern env v
+lcValNoPat :: LinearEnv -> Value -> Result LinearEnv
+lcValNoPat env ValUnit = return env
+lcValNoPat env (ValInt _) = return env
+lcValNoPat env (ValVar var) = increEnv env var
+lcValNoPat env (ValLInj v) = lcValNoPat env v
+lcValNoPat env (ValRInj v) = lcValNoPat env v
+lcValNoPat env (ValPair l r) = do
+  env' <- lcValNoPat env l
+  lcValNoPat env' r
+lcValNoPat env (ValAnn v _) = lcValNoPat env v
 
 {-- Helper functions --}
-increEnv :: LinearEnv -> String -> LinearEnv
-increEnv [] var = error $ show var ++ " not found!"
+increEnv :: LinearEnv -> String -> Result LinearEnv
+increEnv [] var = fail $ show var ++ " not found!"
 increEnv ((var',n):tl) var
-  | var == var' =
+  | var' == var =
       if n > 0
-      then error $ "Variable " ++ show var ++ " is used more than once!"
-      else ((var', (n + 1)):tl)
-increEnv ((var',n):tl) var = (var',n):(increEnv tl var)
+      then fail $ "Variable " ++ show var ++ " is used more than once!"
+      else return ((var', (n + 1)):tl)
+increEnv ((var',n):tl) var = do
+  env' <- increEnv tl var
+  return $ (var',n):env'
 
-extendPat :: LinearEnv -> Pattern -> LinearEnv
-extendPat env (PtSingleVar var) = (var, 0):env
-extendPat env (PtMultiVar vars) = (map (\x -> (x , 0)) vars) ++ env
+extendPat :: LinearEnv -> Pattern -> Result LinearEnv
+extendPat env (PtSingleVar var) = return $ (var, 0):env
+extendPat env (PtMultiVar vars) = return $ (map (\x -> (x , 0)) vars) ++ env
 
-dropPat :: LinearEnv -> Pattern -> LinearEnv
-dropPat (_:tl) (PtSingleVar _) = tl
-dropPat env (PtMultiVar []) = env
-dropPat (_:tl) (PtMultiVar (_:vars)) = extendPat tl (PtMultiVar vars)
-dropPat env pat = error $ "Environment " ++ show env ++ " doesn't have enough variables for" ++ show pat ++ "!"
+dropPat :: LinearEnv -> Pattern -> Result LinearEnv
+dropPat (_:tl) (PtSingleVar _) = return tl
+dropPat env (PtMultiVar []) = return env
+dropPat (_:tl) (PtMultiVar (_:vars)) = dropPat tl (PtMultiVar vars)
+dropPat env pat = fail $ "Environment " ++ show env ++ " doesn't have enough variables for" ++ show pat ++ "!"
