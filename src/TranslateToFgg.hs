3cde1488639d9f4fe656922eaeff5376958f0805
chihyang <chihyanghsin@gmail.com>
2026-02-03 01:30:39 -0500

    Refactoring in FGG translator.

--- src/TranslateToFgg.hs
+++ src/TranslateToFgg.hs
@@ -26,7 +26,7 @@ err :: String -> Result a
 err msg = reportErr moduleName msg
 
 errT :: String -> ResultT a
-errT msg = throwError $ moduleName ++ msg
+errT msg = throwError msg
 
 type Info = Map.Map String Iso
 
@@ -55,8 +55,8 @@ compileToFggPg (defs, pg) =
        Right _ -> return (rulesToFGG domain edgeLabel nodeLabels rs)
 
 prog2NodeLabel :: Program -> Result [NodeLabel]
-prog2NodeLabel (PgTm (TmAnn _ t)) = return $ map snd (newOutNodes [t])
-prog2NodeLabel (PgIs (IsoAnn _ (ITyBase ty1 ty2))) = return $ map snd (newOutNodes [ty1, ty2])
+prog2NodeLabel (PgTm (TmAnn _ t)) = return $ map snd (newOutTmNodes [t])
+prog2NodeLabel (PgIs (IsoAnn _ (ITyBase ty1 ty2))) = return $ map snd (newOutTmNodes [ty1, ty2])
 prog2NodeLabel pg@(PgIs (IsoAnn {})) = err $ "The given iso is not supported to translate to FGG: " ++ show pg
 prog2NodeLabel pg = err $ "The given program is not type annotated: " ++ show pg
 
@@ -73,11 +73,11 @@ prog2Rules _ pg = errT $ "The given program is not type annotated: " ++ show pg
 term2Fgg :: Info -> Term -> BaseType -> ResultT [Node]
 -- term2Fgg _ tm ty | trace ("Convert tm: " ++ show tm ++ " : " ++ show ty) False = undefined
 term2Fgg _ TmUnit BTyUnit =
-  let ns = newOutNodes [BTyUnit] in
+  let ns = newOutTmNodes [BTyUnit] in
     mkTermRule TmUnit ns [Edge ns (ElTerminal (FaMulProd []))] ns []
 term2Fgg _ tm@(TmVar v) ty =
-  let varNs = newVarNames [(v, ty)]
-      tyNs = newOutNodes [ty]
+  let varNs = newVarTmNames [(v, ty)]
+      tyNs = newOutTmNodes [ty]
       ns = varNs ++ tyNs
   in
     mkTermRule tm ns [Edge ns (ElTerminal (FaIdentity ty))] ns varNs
@@ -85,8 +85,8 @@ term2Fgg info tm@(TmPair tm1 tm2) ty@(BTyProd ty1 ty2) =
   do varsNs1 <- term2Fgg info tm1 ty1
      varsNs2 <- term2Fgg info tm2 ty2
      let varsNs = varsNs1 ++ varsNs2
-         [internalNs1, internalNs2] = newInternalNodes [ty1, ty2]
-         tyNs = newOutNodes [ty]
+         [internalNs1, internalNs2] = newInternalTmNodes [ty1, ty2]
+         tyNs = newOutTmNodes [ty]
          e1 = termEdge (varsNs1 ++ [internalNs1]) tm1
          e2 = termEdge (varsNs2 ++ [internalNs2]) tm2
          e3 = Edge ([internalNs1, internalNs2] ++ tyNs) (ElTerminal (FaMulProd [ty1, ty2]))
@@ -94,16 +94,16 @@ term2Fgg info tm@(TmPair tm1 tm2) ty@(BTyProd ty1 ty2) =
 term2Fgg info tm@(TmLInj tm1) ty@(BTySum ty1 ty2) =
   do
     varsNs <- term2Fgg info tm1 ty1
-    let internalNs = newInternalNodes [ty1]
-        tyNs = newOutNodes [ty]
+    let internalNs = newInternalTmNodes [ty1]
+        tyNs = newOutTmNodes [ty]
         e1 = termEdge (varsNs ++ internalNs) tm1
         e2 = Edge (internalNs ++ tyNs)  (ElTerminal (FaAddProd [ty1, ty2] 0))
     mkTermRule tm (varsNs ++ internalNs ++ tyNs) [e1, e2] (varsNs ++ tyNs) varsNs
 term2Fgg info tm@(TmRInj tm2) ty@(BTySum ty1 ty2) =
   do
     varsNs <- term2Fgg info tm2 ty2
-    let internalNs = newInternalNodes [ty2]
-        tyNs = newOutNodes [ty]
+    let internalNs = newInternalTmNodes [ty2]
+        tyNs = newOutTmNodes [ty]
         e1 = termEdge (varsNs ++ internalNs) tm2
         e2 = Edge (internalNs ++ tyNs)  (ElTerminal (FaAddProd [ty1, ty2] 1))
     mkTermRule tm (varsNs ++ internalNs ++ tyNs) [e1, e2] (varsNs ++ tyNs) varsNs
@@ -111,8 +111,8 @@ term2Fgg info tapp@(TmIsoApp (IsoAnn iso ti@(ITyBase ty1 ty2)) tm) ty2' | ty2 ==
   do
     isoNs <- iso2Fgg info iso ti
     varsNs <- term2Fgg info tm ty1
-    let tyNs1 = newInternalNodes [ty1]
-        tyNs2 = newOutNodes [ty2]
+    let tyNs1 = newInternalTmNodes [ty1]
+        tyNs2 = newOutTmNodes [ty2]
         e1 = termEdge (varsNs ++ tyNs1) tm
         e2 = isoEdge (isoNs ++ tyNs1 ++ tyNs2) iso
     mkTermRule tapp (varsNs ++ isoNs ++ tyNs1 ++ tyNs2) [e1, e2] (varsNs ++ tyNs2) varsNs
@@ -122,8 +122,8 @@ term2Fgg info tlet@(TmLet pat (TmAnn rhs ty1) body) ty =
     varsNs2 <- pattern2fgg info pat ty1
     varsNs3 <- term2Fgg info body ty -- we assume that varsNs2 is a subset of varsNs3
     let varsNs = filter (`notElem` varsNs2) (varsNs1 ++ varsNs3)
-        tyNs1 = newInternalNodes [ty1]
-        tyNs = newOutNodes [ty]
+        tyNs1 = newInternalTmNodes [ty1]
+        tyNs = newOutTmNodes [ty]
         e1 = termEdge (varsNs1 ++ tyNs1) rhs
         e2 = termEdge (varsNs2 ++ tyNs1) (pat2Tm pat)
         e3 = termEdge (varsNs3 ++ tyNs) body
@@ -131,7 +131,7 @@ term2Fgg info tlet@(TmLet pat (TmAnn rhs ty1) body) ty =
 term2Fgg info t@(TmScale s tm) ty =
   do
     varsNs <- term2Fgg info tm ty
-    let tyNs = newOutNodes [ty]
+    let tyNs = newOutTmNodes [ty]
         ns = varsNs ++ tyNs
         e1 = Edge [] (ElTerminal (FaScalar s))
         e2 = termEdge ns tm
@@ -146,13 +146,20 @@ iso2Fgg :: Info -> Iso -> IsoType -> ResultT [Node]
 iso2Fgg info iso@(IsoValue ves) (ITyBase vTy eTy) = do
   (varsNs, hgfs) <- mkHGFs (cls2Hgf info) ves (vTy, eTy)
   bindIsoCases varsNs iso hgfs
-iso2Fgg info (IsoVar var) ti@(ITyBase vTy eTy) =
-  do
-    iso <- except (lookupIso var info)
-    _ <- iso2Fgg info iso ti
-    let ns = newOutNodes [vTy, eTy]
-        e1 = isoEdge ns iso
-    mkIsoRule (IsoVar var) ns [e1] ns []
+iso2Fgg info (IsoVar var) ti@(ITyBase vTy eTy) = do
+  iso <- except (lookupIso var info)
+  _ <- iso2Fgg info iso ti
+  let ns = newOutTmNodes [vTy, eTy]
+      e1 = isoEdge ns iso
+  mkIsoRule (IsoVar var) ns [e1] ns []
+iso2Fgg info (IsoVar var) ti =
+  errT $ "Translating second order iso var to FGG is in progress: " ++ var
+iso2Fgg info iso@(IsoLam var lTy rTy body) ty =
+  errT $ "Translating second order iso to FGG is in progress: " ++ show iso
+iso2Fgg info iso@(IsoApp (IsoAnn lhs tiso@(ITyFun lhsTy rhsTy _)) rhs) ty = do
+  lhsVars <- iso2Fgg info lhs tiso
+  rhsVars <- iso2Fgg info rhs (ITyBase lhsTy rhsTy)
+  errT $ "Translating application of second-order iso to FGG is in progress: " ++ show iso
 iso2Fgg info (IsoAnn iso ty) ty' | ty == ty' = iso2Fgg info iso ty
 iso2Fgg _ iso _ = errT $ "Unsupported iso for translating to FGG: " ++ show iso
 
@@ -164,7 +171,7 @@ cls2Hgf info (v, e) (vTy, eTy) =
     varsNs1 <- term2Fgg info vTm vTy
     varsNs2 <- term2Fgg info eTm eTy
     let varsNs = varsNs1 ++ varsNs2
-        [tyNs1, tyNs2] = newOutNodes [vTy, eTy]
+        [tyNs1, tyNs2] = newOutTmNodes [vTy, eTy]
         ns = varsNs ++ [tyNs1, tyNs2]
         e1 = termEdge (varsNs1 ++ [tyNs1]) vTm
         e2 = termEdge (varsNs2 ++ [tyNs2]) eTm
@@ -182,7 +189,7 @@ mkHGFs f as b = foldlM acc ([], []) as where
 tm2Hgf :: Info -> Term -> BaseType -> ResultT ([Node], [HGF])
 tm2Hgf info tm ty = do
   varsNs <- term2Fgg info tm ty
-  let tyNs = newOutNodes [ty]
+  let tyNs = newOutTmNodes [ty]
       ns = varsNs ++ tyNs
       e1 = termEdge ns tm
   mkHGF ns [e1] ns varsNs
@@ -246,7 +253,8 @@ domainIsoType (ITyBase vTy eTy) = do
   eVals <- domainBaseType eTy
   -- We use product type to enumerate all possible base values
   return $ [PBValPair l r | l <- vVals, r <- eVals]
-domainIsoType ty = err $ "Unsupported ISO type to converting to a FGG domain: " ++ show ty
+domainIsoType ty@(ITyFun lhsTy rhsTy bodyTy) =
+  err $ "Getting domain of higher-order iso type is in progress: " ++ show ty
 
 mkDomain :: [ProgramBaseValue] -> Domain
 mkDomain vals = Domain (length vals) (map (DValue . show) vals)
@@ -356,14 +364,30 @@ unwrapExp (ExpLet pat iso pat' body) = ExpLet pat (unwrapIso iso) pat' (unwrapEx
 unwrapExp (ExpScale s e) = ExpScale s (unwrapExp e)
 unwrapExp (ExpPlus es) = ExpPlus (map unwrapExp es)
 
-{- mkVarNodes tm
+{- Functions to make nodes. -}
+newInternalNodes :: (a -> NodeLabel) -> [a] -> [Node]
+newInternalNodes f as = [(NnInternal j, f atp) | (j, atp) <- enumerate as]
 
-   Make internal nodes from free variables in a term. -}
-newInternalNodes :: [BaseType] -> [Node]
-newInternalNodes as = [(NnInternal j, TmNodeLabel atp) | (j, atp) <- enumerate as]
+newVarNames :: (a -> NodeLabel) -> [(String, a)] -> [Node]
+newVarNames f vtys = [ (NnVar v, f ty) | (v, ty) <- vtys]
 
-newVarNames :: [(String, BaseType)] -> [Node]
-newVarNames vtys = [ (NnVar v, TmNodeLabel ty) | (v, ty) <- vtys]
+newOutNodes :: (a -> NodeLabel) -> [a] -> [Node]
+newOutNodes f tys = [(NnOut j, f ty) | (j, ty) <- enumerate tys]
 
-newOutNodes :: [BaseType] -> [Node]
-newOutNodes tys = [(NnOut j, TmNodeLabel ty) | (j, ty) <- enumerate tys]
+newInternalTmNodes :: [BaseType] -> [Node]
+newInternalTmNodes as = newInternalNodes TmNodeLabel as
+
+newVarTmNames :: [(String, BaseType)] -> [Node]
+newVarTmNames vtys = newVarNames TmNodeLabel vtys
+
+newOutTmNodes :: [BaseType] -> [Node]
+newOutTmNodes tys = newOutNodes TmNodeLabel tys
+
+newInternalIsoNodes :: [IsoType] -> [Node]
+newInternalIsoNodes as = newInternalNodes IsoNodeLabel as
+
+newVarIsoNames :: [(String, IsoType)] -> [Node]
+newVarIsoNames vtys = newVarNames IsoNodeLabel vtys
+
+newOutIsoNodes :: [IsoType] -> [Node]
+newOutIsoNodes tys = newOutNodes IsoNodeLabel tys
